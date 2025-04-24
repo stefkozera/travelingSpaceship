57a05193ec7108bc71d9ba6cfb42869cbe044c65
stefkozera <stefkozera@gmail.com>
2025-04-24 14:57:37 -0400

    updates

--- travelingSpaceship.cpp
+++ travelingSpaceship.cpp
@@ -2,25 +2,25 @@
 #include <stdlib.h>
 #include <math.h>
 #include <random>
+#include <algorithm>
 
 #define NUM_TARGETS 35
-#define POPULATION 100
+#define PARENTS 100
 #define GENERATIONS 500
 #define MAX_DISTANCE 1000
-#define MUTATIONRATE 50 //Out of 100
+#define MUTATIONRATE 10 //Out of 100
 
-#define HALF_POPULATION POPULATION/2
+#define HALF_PARENTS PARENTS/2
 
 static std::random_device rd;
 static std::mt19937 rng(rd()); 
 std::uniform_int_distribution<int> targetsRand(0, NUM_TARGETS - 1);
 std::uniform_int_distribution<int> pivotRand(0, NUM_TARGETS - 2);
 std::uniform_int_distribution<int> distanceRand(0, MAX_DISTANCE);
-std::uniform_int_distribution<int> crossoverRand(0, HALF_POPULATION - 1);
+std::uniform_int_distribution<int> crossoverRand(0, HALF_PARENTS - 1);
 std::uniform_int_distribution<int> mutateRand(1, 100);
 
-
-void generateCities(int **f_targets)
+void generateCities(int (&f_targets)[NUM_TARGETS][3])
 {
     for(int i = 0; i < NUM_TARGETS; ++i){
         for(int j = 0; j < 3; ++j){
@@ -29,7 +29,8 @@ void generateCities(int **f_targets)
     }
 }
 
-void calcDistanceMatrix(int **f_targets, double **f_targetDistance)
+void calcDistanceMatrix(int (&f_targets)[NUM_TARGETS][3], 
+                        double (&f_targetDistance)[NUM_TARGETS][NUM_TARGETS])
 { 
     double xDist, yDist, zDist;
 
@@ -49,18 +50,18 @@ void calcDistanceMatrix(int **f_targets, double **f_targetDistance)
     }
 }
 
-void generateInitPopulation(int **population)
+void generateInitPopulation(int (&population)[PARENTS][NUM_TARGETS])
 {
     int temp, num;
 
     //Seed initial population with values in order, will go to shuffle function
-    for(int i = 0; i < POPULATION; ++i){
+    for(int i = 0; i < PARENTS; ++i){
         for (int j = 0; j < NUM_TARGETS; ++j){
             population[i][j] = j;
         }
     }
 
-    for(int i = 0; i < POPULATION; ++i){
+    for(int i = 0; i < PARENTS; ++i){
         for (int j = NUM_TARGETS - 1; j > 0; --j) {
             num = targetsRand(rng);
             temp = population[i][num];
@@ -69,13 +70,14 @@ void generateInitPopulation(int **population)
         }
     }
 
-    for(int i = 0; i < POPULATION; ++i){
+    for(int i = 0; i < PARENTS; ++i){
         for (int j = 0; j < NUM_TARGETS; ++j){
         }
     }
 }
 
-double fitnessCalc(int *f_individual, double **f_targetDistance)
+double fitnessCalc(int *f_individual, 
+                   double (&f_targetDistance)[NUM_TARGETS][NUM_TARGETS])
 {
     double totalDistance = 0;
 
@@ -137,38 +139,28 @@ void mutate(int *f_child)
 
 int main()
 {
-
-    /* Data allocation - Not embedded safe yet */
-    /////////////////////////////////////////////
-
     // Targets array, NUM_TARGETS x 3 ()
-    int **targets = new int*[NUM_TARGETS];
-    for(int i = 0; i < NUM_TARGETS; ++i){
-        targets[i] = new int[3];
-    }
+    int targets[NUM_TARGETS][3] = { { } };
 
-     // Distances between all targets, NUM_TARGETS x NUM_TARGETS
-    double **targetDistance = new double*[NUM_TARGETS];
-    for(int i = 0; i < NUM_TARGETS; ++i){
-        targetDistance[i] = new double[NUM_TARGETS];
-    }
+    // Distances between all targets, NUM_TARGETS x NUM_TARGETS
+    double targetDistance[NUM_TARGETS][NUM_TARGETS] = { { } };
 
-    // Initial population array, POPULATION x NUM_TARGETS
-    int **population = new int*[POPULATION];
-    for(int i = 0; i < POPULATION; ++i){
-        population[i] = new int[NUM_TARGETS];
-    }
+    // Initial population array, PARENTS x NUM_TARGETS
+    int population[PARENTS][NUM_TARGETS] = { { } };
 
     // Selection Population, POPULATION x NUM_TARGETS
     // Top half is best 50% from population
     // Bottom half is children obtained from crossover
-    int **selectionPop = new int*[POPULATION];
-    for(int i = 0; i < POPULATION; ++i){
-        selectionPop[i] = new int[NUM_TARGETS];
-    }
+    int selectionPop[PARENTS][NUM_TARGETS] = { { } };
 
     // One single created child population, NUM_TARGETS
-    int *child = new int[NUM_TARGETS];
+    int child[NUM_TARGETS] = { };
+
+    std::vector<std::pair<double, int> > fitnessPop;
+    fitnessPop.reserve(PARENTS);
+    for(int i = 0; i < PARENTS; ++i){
+        fitnessPop.push_back(std::make_pair(0, i));
+    }
 
     /* Initialization */
     //////////////////////////////////////////////////
@@ -184,12 +176,11 @@ int main()
 
         //vector pair, with fitnesses of entire population, and indexes of those fitnesses location
         //when compared to original population
-        std::vector<std::pair<double, int> > fitnessPop;
         double bestFitness;
 
-        for (int i = 0; i < POPULATION; ++i){
+        for (int i = 0; i < PARENTS; ++i){
             double fitness = fitnessCalc(population[i], targetDistance);
-            fitnessPop.push_back(std::make_pair(fitness, i));
+            fitnessPop[i] = std::make_pair(fitness, i);
 
             if (i == 0){
                 bestFitness = fitness;
@@ -203,18 +194,15 @@ int main()
 
         printf("Gen: %d, Best Fitness: %f\n", gen, bestFitness);
 
-        std::nth_element(fitnessPop.begin(), fitnessPop.begin() + HALF_POPULATION, fitnessPop.end());
-        
-        for(int i = 0; i < POPULATION; ++i){
-            //printf("%f %d \n", fitnessPop[i].first, fitnessPop[i].second);
-        }
-        //printf("\n");
+        std::nth_element(fitnessPop.begin(), fitnessPop.begin() + HALF_PARENTS, fitnessPop.end());
 
-        for (int i = 0; i < HALF_POPULATION; ++i){
-            selectionPop[i] = population[fitnessPop[i].second];
+        for (int i = 0; i < HALF_PARENTS; ++i){
+            for(int j = 0; j < NUM_TARGETS; ++j){
+                selectionPop[i][j] = population[fitnessPop[i].second][j];
+            }
         }
 
-        for(int i = HALF_POPULATION; i < POPULATION; ++i)
+        for(int i = HALF_PARENTS; i < PARENTS; ++i)
         {
             int parent1ind = crossoverRand(rng);
             int parent2ind = crossoverRand(rng);
@@ -227,22 +215,18 @@ int main()
             }
 
         }
-        //printf("\n");
 
-        for(int i = 0; i < POPULATION; ++i){
+        for(int i = 0; i < PARENTS; ++i){
             for(int j = 0; j < NUM_TARGETS; ++j)
             {
                 population[i][j] = selectionPop[i][j];
-                //printf("%d ", population[i][j]);
             }
-            //printf("\n");
         }
-        //printf("\n");
     }
 
     double finalFitness;
 
-    for (int i = 0; i < POPULATION; ++i){
+    for (int i = 0; i < PARENTS; ++i){
         double fitness = fitnessCalc(population[i], targetDistance);
 
         if (i == 0)
@@ -257,5 +241,6 @@ int main()
             }
         }
     }
+
     printf("Final Fitness: %f\n", finalFitness);
 }
